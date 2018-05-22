a699fcc96ed970021c72b933b12adc81fcfba5b3
Vadim Melnikov <Vadimvadic_m@mail.ru>
2018-05-22 03:31:52 +0300

    first component

--- app/js/vue.js
+++ app/js/vue.js
@@ -1,3 +1,15 @@
+Vue.component('task', {
+    template: `
+    <ul class="list-group">
+        <li class="list-group-item">{{ msg }}</li>
+    </ul>
+    `,
+    data(){
+        return {
+            msg: 'hello'
+        }
+    }
+})
 var app = new Vue({
     el: '#vue',
     data: {
@@ -7,28 +19,42 @@ var app = new Vue({
         names: ['vadim', 'elena', 'carim', 'lolka'],
         value: '',
         isLoading: false,
-        move: false
+        move: false,
+        tasks: [
+            {description: 'Hello2', completed: true},
+            {description: 'Hello3', completed: false},
+            {description: 'Hello4', completed: true},
+            {description: 'Hello5', completed: false},
+            {description: 'Hello6', completed: false}
+        ]
     },
 
-    mounted(){
+    computed: {
+        inclementTasks() {
+            return this.tasks.filter(task => !task.completed);
+        }
+    },
+
+    mounted() {
 
     },
     methods: {
-        addName(){
+        addName() {
             if (this.value.length > 0) {
                 this.names.push(this.value);
                 app.value = '';
-            } else{
+            } else {
                 alert('It is pure')
             }
         },
 
-        moveMouse(){
+        moveMouse() {
             this.move = !this.move;
         },
 
-        toggleClass(){
+        toggleClass() {
             this.isLoading = !this.isLoading;
         }
     }
 })
+
