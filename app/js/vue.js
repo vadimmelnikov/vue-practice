83fb2f79d574ba7f2566b94cb1edbf28745a62bb
Vadim Melnikov <Vadimvadic_m@mail.ru>
2018-05-24 02:54:22 +0300

    modals

--- app/js/vue.js
+++ app/js/vue.js
@@ -16,7 +16,51 @@ Vue.component('task-list', {
             ]
         }
     }
-})
+});
+Vue.component('message', {
+    props: ['title', 'body'],
+    data(){
+        return{
+            isVisible: true
+        }
+    },
+    template: `
+    <div class="alert alert-warning alert-dismissible fade show" role="alert" v-show="isVisible">
+      <h4 class="alert-heading">{{ title }}</h4>
+      <p>{{ body }}</p>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true" @click="!isVisible">&times;</span>
+      </button>
+    </div>`,
+
+
+});
+Vue.component('modal', {
+    template: `
+    <div class="modal fade show" tabindex="-1" role="dialog" id="mdl">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="$emit('close')">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="$emit('close')">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `,
+    mounted(){
+        $('.modal').modal('show');
+    }
+});
 var app = new Vue({
     el: '#vue',
     data: {
@@ -27,6 +71,7 @@ var app = new Vue({
         value: '',
         isLoading: false,
         move: false,
+        showModal: false,
         tasks: [
             {description: 'Hello2', completed: true},
             {description: 'Hello3', completed: false},
